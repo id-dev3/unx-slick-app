4dff31fb3e59070f732847a3a3707041622c3e64
BlackFury117 <idrissadiallo97@icloud.com>
2024-01-27 14:24:39 +0000

    Fixing the mobile view layout

--- script.js
+++ script.js
@@ -26,3 +26,21 @@ if (!motionMatchMedia.matches) {
 }
 
 /// END 3D EFFECT ///
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburgerCheckbox = document.querySelector(".hamburger input");
+  const navLinks = document.querySelector(".nav-links");
+
+  hamburgerCheckbox.addEventListener("change", function () {
+    if (this.checked) {
+      navLinks.style.display = "block";
+      document.body.style.overflow = "hidden";
+      navLinks.classList.add("show");
+    } else {
+      navLinks.style.display = "none";
+      document.body.style.overflow = "auto";
+      navLinks.classList.remove("show");
+    }
+  });
+});
